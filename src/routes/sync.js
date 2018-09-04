b5597b03631dcce6d36e74c2806ed432d3f952b2
sr229 <aguilarralph13@gmail.com>
2018-09-04 21:42:32 +0800

    compress getType function

--- src/routes/sync.js
+++ src/routes/sync.js
@@ -11,26 +11,18 @@ const https = require("https");
  
 router.post("/reposync", async (req, res) => {
     const atts = req.body.attachments;
-    if (atts.length <= 0)
-        return res.send("No attachments.");
+    // basic sanity check
+    if (atts.length <= 0) return res.send("No attachments.");
+
     atts.map(att => att.url).forEach(async url => {
         // get url -> upload to WebDAV
         const mimeType = getType(url);
         https.get(url, res => {
             res.once("data", chunk => {
-                webdav.putFileContents("", chunk, {format: mimeType});
+                webdav.putFileContents("", chunk, {format: ft(chunk).mime});
             });
         });
     });
 });
 
-
-function getType(url) {
-    https.get(url, res => {
-        res.once("data", chunk => {
-            res.destroy();
-            return ft(chunk).mime;
-        });
-    });
-}
 module.exports = router;
